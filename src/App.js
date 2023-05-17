bb7767529b8dae055d0d5a96da422081a1e28f51
DIVIYASH.R <diviyash04jun2002@gmail.com>
2023-05-17 12:11:14 +0530

    user profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const player = [
+    {name :'Messi',
+    pic : "https://therealchamps.com/wp-content/uploads/getty-images/2017/07/1475698105.jpeg",
+    rating : '4.5 / 5' 
+  },
+    {name : 'CR7',
+    pic : "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltee886584fd1cd345/60db8718892a730f5883a347/68a712aa7ffd3da7d91c2fe1469bda58990f7ab7.jpg?auto=webp&format=pjpg&width=1080&quality=60",
+    rating : 4.5 / 5
+  
+  },
+    {name : 'Mappi',
+     pic : "https://therealchamps.com/wp-content/uploads/getty-images/2017/07/1475698105.jpeg"
+  },
+    {name : 'Neymer Jr',
+     pic : "https://images.ctfassets.net/3mv54pzvptwz/5eTv6hTyA1pqkFClYRn0qt/be626a573cbf1ee7e421b73f87ed6851/20221205_foto_GETTY_neymar_jr_jogo_brasil_x_coreia_copa_do_mundo__209_.jpg"
+  }]
+
+  return(
+    <div>
+
+      { /* Type 1 <UserName name = 'Messi'/>
+      <UserName name = 'CR7'/>
+      <UserName name = 'Neymar'/> */}
+
+     {/*Type 2 {player.map((nm) => <UserName name= {nm}/>)} */}
+
+     {player.map((nm) => <UserName name={nm.name} pic={nm.pic} />)}
+
+
     </div>
   );
 }
+// function UserName({name}){
+//   return(
+//     <div>
+//     <img className="profile-img"
+//      src="https://therealchamps.com/wp-content/uploads/getty-images/2017/07/1475698105.jpeg" alt= {name}></img>
+//     <h1 className='userName'>Chaos,{name}!😎👌</h1>  {/* Interpolation syntax, Substitution , Template syntax*/}
+     
+//      </div>
+//   );
+// }
 
-export default App;
+function UserName({name, pic, rating}){
+  return(
+    <div>
+    <legend>Player</legend>
+    <img className="profile-img"
+     src={pic} alt= {name}></img>
+    <h1 className='userName'>Chaos,{name}!😎👌</h1>  {/* Interpolation syntax, Substitution , Template syntax*/}
+    
+     
+     </div>
+  );
+}
